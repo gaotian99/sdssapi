1d93f287601989d68137630a63fb8d910e8b64a9
Dominic <domleggio@gmail.com>
2019-01-18 19:19:19 -0800

    registration and login working

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 // User.js
 var mongoose = require('mongoose'); 
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
 var UserSchema = new mongoose.Schema({  
   email: {type: String, unique: false, required: true },
@@ -11,6 +13,30 @@ var UserSchema = new mongoose.Schema({
   phoneNumber:{type: String,required: true,},
   role:{type: String, required: true,},
 });
+
+UserSchema.methods.setPassword = function(password){
+  this.passwordSalt = crypto.randomBytes(16).toString('hex');
+  this.passwordHash = crypto.pbkdf2Sync(password, this.passwordSalt, 1000, 64, 'sha512').toString('hex');
+};
+
+UserSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.passwordSalt, 1000, 64, 'sha512').toString('hex');
+  
+  return this.passwordHash === hash;
+  
+};
+
+UserSchema.methods.generateJwt = function() {
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt(expiry.getTime() / 1000),
+  }, "MY_SECRET"); // DO NOT KEEP YOUR SECRET IN THE CODE!
+};
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var jwt = require('express-jwt');
+var auth = jwt({
+  secret: 'MY_SECRET',
+  userProperty: 'payload'
+});
+
+var ctrlProfile = require('../controllers/profileController');
+var ctrlAuth = require('../controllers/authenticationController');
+
+// profile
+router.get('/profile', auth, ctrlProfile.profileRead);
+
+// authentication
+ //router.post('/register', ctrlAuth.register);
+ router.post('/login', ctrlAuth.login);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
